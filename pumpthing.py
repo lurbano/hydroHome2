6ab60c22f7a512edac8ba27d756a20e276fb5952
GeorgeDouglass-Gamer <gdouglass@tfssa.org>
2021-04-28 14:38:24 -0500

    should work

--- pumpthing.py
+++ pumpthing.py
@@ -3,20 +3,23 @@ import time
 
 in1 = 16
 
+pumpRate=25
+VolNeeded=21000
+pumpTime= VolNeeded/pumpRate
+secondsneeded=(60*60*24)-pumpTime
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1, GPIO.OUT)
 
+while True:
+    GPIO.output(in1, True)
+    time.sleep(pumpTime)
+    GPIO.output(in1, False)
+    print("PumPiNg nOW")
+    time.sleep(secondsneeded)
 
-GPIO.output(in1, False)
-
-
-GPIO.output(in1, True)
-time.sleep(5)
-GPIO.output(in1, False)
-
-
-
+#25 ml per second per pump
+#trying to fill 21L
 
 
 GPIO.cleanup()

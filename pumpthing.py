15a61ddb6d32c45883b98288a160d8410b6393a5
GeorgeDouglass-Gamer <gdouglass@tfssa.org>
2021-05-04 14:23:49 -0500

    b

--- pumpthing.py
+++ pumpthing.py
@@ -9,6 +9,7 @@ timestorun=4
 numpix = 20
 t=.03
 t2=.05
+pixels = neopixel.NeoPixel(board.D18, numpix)
 
 #pump stuff
 in1 = 16
@@ -17,7 +18,7 @@ VolNeeded=21000
 pumpTime= VolNeeded/pumpRate
 secondsneeded=(60*60*24)-pumpTime
 
-elapsedT=0
+elapsedT =0
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1, GPIO.OUT)
 

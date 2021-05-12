9f638606eb08310293c9ecacea1d79b697bf3855
dorthy99 <mhahn@tfssa.org>
2021-05-12 15:15:27 -0500

    server setup: t=25

---
+++ webServer/server.py
@@ -0,0 +1,128 @@
+#! /usr/bin/python3
+
+# From: https://www.hackster.io/dataplicity/control-raspberry-pi-gpios-with-websockets-af3d0c
+
+import os.path
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+import tornado.gen
+import RPi.GPIO as GPIO
+import time
+import subprocess
+import json
+import sys
+import argparse
+import asyncio
+#from numpy import arange, mean
+import numpy as np
+#from oledU import *
+
+
+#Tornado Folder Paths
+settings = dict(
+	template_path = os.path.join(os.path.dirname(__file__), "templates"),
+	static_path = os.path.join(os.path.dirname(__file__), "static")
+	)
+
+#pyPath = '/home/pi/rpi-led-strip/pyLED/'
+
+#Tonado server port
+PORT = 8040
+
+class MainHandler(tornado.web.RequestHandler):
+	def get(self):
+		print ("[HTTP](MainHandler) User Connected.")
+		self.render("index.html")
+
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+	def open(self):
+		print ('[WS] Connection was opened.')
+		self.write_message('{"who": "server", "info": "on"}')
+		#self.oled = oledU(128,32)
+
+
+	async def on_message(self, message):
+		print ('[WS] Incoming on_message:', message)
+		try:
+			msg = json.loads(message)
+			if msg["what"] == "server":
+				if msg["opts"] == "off":
+					sys.exit("Stopping server")
+
+			# LED STRIP (2/3)
+
+
+			if msg["what"] == "Temperature":
+				print("checking temperature ")
+				t = 25
+				self.write_message({"info":"temperature", "value":t})
+
+
+
+
+
+			if msg["what"] == "restart":
+
+				subprocess.Popen('sleep 5 ; sudo python3 '+os.path.join(os.path.dirname(__file__), "server.py" + f' -n {nPix}'), shell=True)
+				main_loop.stop()
+
+			if msg["what"] == "reboot":
+
+				subprocess.Popen('sleep 5 ; sudo reboot', shell=True)
+				main_loop.stop()
+
+
+
+		except Exception as e:
+			print(e)
+			print("Exception: Error with data recieved by server")
+			print(message)
+
+
+	def on_close(self):
+		print ('[WS] Connection was closed.')
+
+
+
+application = tornado.web.Application([
+  (r'/', MainHandler),
+  (r'/ws', WSHandler),
+  ], **settings)
+
+
+if __name__ == "__main__":
+	try:
+		http_server = tornado.httpserver.HTTPServer(application)
+		http_server.listen(PORT)
+		print("hello")
+		main_loop = tornado.ioloop.IOLoop.instance()
+
+		print ("Tornado Server started")
+
+
+
+		# LED STRIP (END)
+
+		# get ip address
+		cmd = "hostname -I | cut -d\' \' -f1"
+		IP = subprocess.check_output(cmd, shell=True).decode("utf-8")
+		print('IP: '+ IP +":" + str(PORT))
+		#oled.write('IP: '+ IP, 3)
+		cmd = 'iwgetid | sed \'s/.*://\' | sed \'s/"//g\''
+		wifi = subprocess.check_output(cmd, shell=True).decode("utf-8")
+		#oled.write(wifi, 2)
+		print(wifi)
+
+		main_loop.start()
+
+
+
+
+	except:
+		print ("Exception triggered - Tornado Server stopped.")
+
+
+#End of Program
---
+++ webServer/static/ws-client.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+        var WEBSOCKET_ROUTE = "/ws";
+
+        if(window.location.protocol == "http:"){
+            //localhost
+            var ws = new WebSocket("ws://" + window.location.host + WEBSOCKET_ROUTE);
+        }
+        else if(window.location.protocol == "https:"){
+            //Dataplicity
+            var ws = new WebSocket("wss://" + window.location.host + WEBSOCKET_ROUTE);
+        }
+
+        ws.onopen = function(evt) {
+            // $("#ws-status").html("Connected");
+            // $("#ws-status").css("background-color", "#afa");
+            // $("#server_light").val("ON");
+            $("#signal").html("READY");
+            $("#ws-status").html("Connected");
+            $("#ws-status").css("background-color", "#afa");
+        };
+
+        ws.onmessage = function(evt) {
+            //console.log(evt);
+            var sData = JSON.parse(evt.data);
+            //console.log(sData);
+            if (sData.info == 'temperature'){
+                $("#tempinfo").html(sData.value);
+              }
+
+        };
+
+        ws.onclose = function(evt) {
+            $("#ws-status").html("Disconnected");
+            $("#ws-status").css("background-color", "#faa");
+            $("#server_light").val("OFF");
+        };
+
+        //MESSAGES TO SEND TO THE SERVER
+
+
+        $("#temp").click(function(){
+            var msg = '{"what": "Temperature"}';
+            ws.send(msg);
+        });
+
+
+        $("#restart").click(function(){
+          let check = confirm("Restart Server?");
+          if (check){
+            var msg = '{"what": "restart"}';
+            ws.send(msg);
+          }
+        });
+        $("#reboot").click(function(){
+            let check = confirm("Reboot Pi?");
+            if (check){
+              var msg = '{"what": "reboot"}';
+              ws.send(msg);
+            }
+        });
+
+
+
+      });
